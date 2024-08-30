cd4cbceeacb904b5681949ebb3799491ec0dd981
Angkita Paul <168302404+AS-PBDeveloper@users.noreply.github.com>
2024-08-30 13:10:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./components/calculator";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Calculator />
+      <p className="developer">
+        Developed BY 👩🏼‍💻 <span>The Project Code</span>
+      </p>
+    </div>
+  );
+};
+
+export default App;
